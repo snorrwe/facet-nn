c2cf20fa76e5efc6ee408e68426c9b85a31ca79a
Daniel Kiss <littlesnorrboy@gmail.com>
2020-11-11 18:38:17 +0100

    add test

--- tests/test_nd_array.py
+++ tests/test_nd_array.py
@@ -1,5 +1,5 @@
 import pytest
-from nd import NdArrayD
+from nd import NdArrayD, make_ndf64
 
 
 def test_ctor():
@@ -7,8 +7,11 @@ def test_ctor():
     arr.set_values([i for i in range(12 * 12 * 12)])
 
 
-def test_setter_raises():
+def test_factory():
+    _arr = make_ndf64([2, 4, 3], [i for i in range(2 * 4 * 3)])
 
+
+def test_setter_raises():
     arr = NdArrayD([12, 12, 12])
     with pytest.raises(ValueError):
         arr.set_values([23])
