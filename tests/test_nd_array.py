6108d58de8ef25858f20961fffde28e8239cb557
Daniel Kiss <littlesnorrboy@gmail.com>
2020-11-21 19:46:01 +0100

    Move nd array proxy to its own impl

--- tests/test_nd_array.py
+++ tests/test_nd_array.py
@@ -1,14 +1,13 @@
 import pytest
-from nd import NdArrayD, make_ndf64
+from nd import NdArrayD
 
 
 def test_ctor():
-    arr = NdArrayD([12, 12, 12])
-    arr.set_values([i for i in range(12 * 12 * 12)])
-
-
-def test_factory():
-    _arr = make_ndf64([2, 4, 3], [i for i in range(2 * 4 * 3)])
+    """
+    smoke test
+    """
+    arr = NdArrayD([12, 12, 12], [i for i in range(12 * 12 * 12)])
+    arr.set_values([i * 2 for i in range(12 * 12 * 12)])
 
 
 def test_setter_raises():
